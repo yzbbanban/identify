c4b86d1b1598750c380f84547abff509c62edd21
yzbbanban <yzbbanban@live.com>
2018-06-05 23:33:59 +0800

    测试 hbaseConn 类--表

--- src/test/java/com/yzbbanban/identify/TestHbaseConn.java
+++ src/test/java/com/yzbbanban/identify/TestHbaseConn.java
@@ -2,17 +2,33 @@ package com.yzbbanban.identify;
 
 import com.yzbbanban.identify.common.HbaseConn;
 import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.Table;
 import org.junit.Test;
+
+import java.io.IOException;
 
 /**
  * Created by brander on 2018/6/5
  */
 public class TestHbaseConn {
     @Test
-    public void getConnTest(){
-        Connection connection= HbaseConn.getHBaseConn();
+    public void getConnTest() {
+        Connection connection = HbaseConn.getHBaseConn();
         System.out.println(connection.isClosed());
         HbaseConn.closeConn();
         System.out.println(connection.isClosed());
+    }
+
+
+    @Test
+    public void getTableTest() {
+        try {
+            Table table = HbaseConn.geTable("US_POTULATION");
+            System.out.println(table.getName().getNameAsString());
+            table.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
     }
 }
